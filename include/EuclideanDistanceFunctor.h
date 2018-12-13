665545ee943b78a11c9bba674e3c57fd179fc5a1
jmlowenthal <jamie.lowenthal@gmail.com>
2018-12-13 22:12:41 +0000

    Reimplement NN heuristic with Boost

---
+++ include/EuclideanDistanceFunctor.h
@@ -0,0 +1,77 @@
+#ifndef EUCLIDEAN_DISTANCE_FUNCTOR_H
+#define EUCLIDEAN_DISTANCE_FUNCTOR_H
+
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/simple_point.hpp>
+
+template<typename Graph, typename T>
+class EuclideanDistanceFunctor {
+private:
+    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+public:
+    typedef std::pair<Vertex, Vertex> key_type;
+    typedef const T reference;
+    typedef T value_type;
+    typedef boost::readable_property_map_tag category;
+    EuclideanDistanceFunctor(const Graph&);
+    const T operator[](std::pair<Vertex, Vertex>) const;
+};
+
+template<typename Graph>
+class EuclideanDistanceFunctor<Graph, float> {
+
+private:
+    
+    const Graph& _g;
+
+    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+
+public:
+
+    typedef std::pair<Vertex, Vertex> key_type;
+    typedef const float reference;
+    typedef float value_type;
+    typedef typename boost::readable_property_map_tag category;
+    
+    EuclideanDistanceFunctor(const Graph& g) : _g(g) {}
+
+    const float operator[](std::pair<Vertex, Vertex> p) const {
+        boost::simple_point<float> first = _g[p.first];
+        boost::simple_point<float> second = _g[p.second];
+        float dx = second.x - first.x;
+        float dy = second.y - first.y;
+        return sqrtf(dx * dx + dy * dy);
+    }
+    
+};
+
+
+template<typename Graph>
+class EuclideanDistanceFunctor<Graph, double> {
+
+private:
+    
+    const Graph& _g;
+
+    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+
+public:
+
+    typedef std::pair<Vertex, Vertex> key_type;
+    typedef const double reference;
+    typedef double value_type;
+    typedef typename boost::readable_property_map_tag category;
+    
+    EuclideanDistanceFunctor(const Graph& g) : _g(g) {}
+
+    const float operator[](key_type p) const {
+        boost::simple_point<double> first = _g[p.first];
+        boost::simple_point<double> second = _g[p.second];
+        double dx = second.x - first.x;
+        double dy = second.y - first.y;
+        return sqrt(dx * dx + dy * dy);
+    }
+    
+};
+
+#endif
---
+++ include/nearest_neighbour_metric_tsp_approx.h
@@ -0,0 +1,100 @@
+#ifndef NEARESTNEIGHBOURTSPSOLVER_H
+#define NEARESTNEIGHBOURTSPSOLVER_H
+
+#ifndef NN_RESOLUTION
+#define NN_RESOLUTION float
+#endif
+
+#include <boost/graph/metric_tsp_approx.hpp>
+#include <boost/property_map/property_map.hpp>
+
+#include "EuclideanDistanceFunctor.h"
+
+template<typename VertexListGraph, typename OutputIterator>
+void nearest_neighbour_metrix_tsp_approx(
+    const VertexListGraph& graph,
+    OutputIterator output
+) {
+    nearest_neighbour_metric_tsp_approx_from_vertex(
+        graph,
+        *vertices(graph).first,
+        EuclideanDistanceFunctor<VertexListGraph, NN_RESOLUTION>(graph),
+        boost::tsp_tour_visitor<OutputIterator>(output)
+    );
+}
+
+template<typename VertexListGraph, typename WeightMap, typename OutputIterator>
+void nearest_neighbour_metrix_tsp_approx(
+    const VertexListGraph& graph,
+    WeightMap weights,
+    OutputIterator output
+) {
+    nearest_neighbour_metric_tsp_approx_from_vertex(
+        graph,
+        *vertices(graph).first,
+        weights,
+        boost::tsp_tour_visitor<OutputIterator>(output)
+    );
+}
+
+template<typename VertexListGraph, typename OutputIterator>
+void nearest_neighbour_metric_tsp_approx_from_vertex(
+    const VertexListGraph& graph,
+    typename boost::graph_traits<VertexListGraph>::vertex_descriptor start,
+    OutputIterator output
+) {
+    nearest_neighbour_metric_tsp_approx_from_vertex(
+        graph,
+        start,
+        EuclideanDistanceFunctor<VertexListGraph, NN_RESOLUTION>(graph),
+        boost::tsp_tour_visitor<OutputIterator>(output)
+    );
+}
+
+template<
+    typename VertexListGraph,
+    typename WeightMap,
+    typename TSPVertexVisitor>
+void nearest_neighbour_metric_tsp_approx_from_vertex(
+    const VertexListGraph& g,
+    typename boost::graph_traits<VertexListGraph>::vertex_descriptor start,
+    WeightMap weight_map,
+    TSPVertexVisitor vis
+) {
+    using namespace boost;
+
+    BOOST_CONCEPT_ASSERT((VertexListGraphConcept<VertexListGraph>));
+    BOOST_CONCEPT_ASSERT((TSPVertexVisitorConcept<TSPVertexVisitor, VertexListGraph>));
+
+    typedef typename graph_traits<VertexListGraph>::vertex_descriptor Vertex;
+    typedef typename graph_traits<VertexListGraph>::vertex_iterator VertexItr;
+
+    // BOOST_CONCEPT_ASSERT((ReadablePropertyMapConcept<WeightMap, std::pair<Vertex, Vertex>));
+
+    VertexItr begin, end;
+    tie(begin, end) = vertices(g);
+    std::vector<Vertex> to_visit(begin, end);
+
+    Vertex current = start;
+    to_visit.erase(std::find(to_visit.begin(), to_visit.end(), current));
+    vis.visit_vertex(current, g);
+
+    while (!to_visit.empty()) {
+        auto best = std::min_element(
+            to_visit.begin(),
+            to_visit.end(),
+            [&current, &weight_map](Vertex a, Vertex b) {
+                typedef typename boost::property_traits<WeightMap>::value_type Distance;
+                Distance a_d = weight_map[{ current, a }];
+                Distance b_d = weight_map[{ current, b }];
+                return a_d < b_d;
+            }
+        );
+        vis.visit_vertex(*best, g);
+        current = *best;
+        to_visit.erase(best);
+    }
+
+}
+
+#endif
