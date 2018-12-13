947ca9d36f42ed1bc8d61402af5465dd099c172b
jmlowenthal <jamie.lowenthal@gmail.com>
2018-12-13 13:12:03 +0000

    Add Boost assertion definitions

---
+++ src/assert.cpp
@@ -0,0 +1,30 @@
+#if defined(BOOST_ENABLE_ASSERT_HANDLER) || ( defined(BOOST_ENABLE_ASSERT_DEBUG_HANDLER) && !defined(NDEBUG) )
+
+#include <stdexcept>
+#include <cstdio>
+#include <boost/assert.hpp>
+#include <boost/format.hpp>
+
+#define ASSERTION_MSG(expr, function, file, line) \
+    boost::str(boost::format("%s %d %s: %s") % file % line % function % expr)
+
+void boost::assertion_failed(
+    char const * expr,
+    char const * function,
+    char const * file,
+    long line
+) {
+    throw std::runtime_error(ASSERTION_MSG(expr, function, file, line));
+}
+
+void boost::assertion_failed_msg(
+    char const * expr,
+    char const * msg,
+    char const * function,
+    char const * file,
+    long line
+) {
+    throw std::runtime_error(ASSERTION_MSG(expr, function, file, line));
+}
+
+#endif
