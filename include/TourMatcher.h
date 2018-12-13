8b825796d80e5e064077cdf6275655dd92694338
jmlowenthal <jamie.lowenthal@gmail.com>
2018-12-13 22:24:16 +0000

    Update TourMatcher for simple_point

--- include/TourMatcher.h
+++ include/TourMatcher.h
@@ -2,33 +2,33 @@
 #define TEST_CIRCULARMATCHER_H
 
 #include "catch.hpp"
-#include "Position2D.h"
+#include <boost/graph/simple_point.hpp>
 
 #ifndef EPSILON
 #define EPSILON 1e-9
 #endif
 
-class TourMatcher : public Catch::MatcherBase< std::vector<Position2D> > {
+class TourMatcher : public Catch::MatcherBase< std::vector<boost::simple_point<float>> > {
 
 private:
 
-    std::vector<Position2D>& a;
+    std::vector<boost::simple_point<float>>& a;
 
-    inline bool equal(Position2D const& a, Position2D const& b) const {
+    inline bool equal(boost::simple_point<float> const& a, boost::simple_point<float> const& b) const {
         return fabsf(a.x - b.x) < EPSILON && fabsf(a.y - b.y) < EPSILON;
     }
 
 public:
 
-    TourMatcher(std::vector<Position2D>& v);
+    TourMatcher(std::vector<boost::simple_point<float>>& v);
 
-    virtual bool match(std::vector<Position2D> const& b) const override;
+    virtual bool match(std::vector<boost::simple_point<float>> const& b) const override;
     
     virtual std::string describe() const;
 
 };
 
-inline TourMatcher TourEqual(std::vector<Position2D>& v) {
+inline TourMatcher TourEqual(std::vector<boost::simple_point<float>>& v) {
     return TourMatcher(v);
 }
 
