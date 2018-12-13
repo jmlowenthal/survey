e28b50985ef06e8c3fc503265e97d896275a7c6b
jmlowenthal <jamie.lowenthal@gmail.com>
2018-12-13 14:47:43 +0000

    Remove test in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,5 @@
-#include "AntColonySystem.h"
 #include <stdio.h>
 
 int main() {
-    std::vector<Position2D> points = {
-        Position2D(3, 0),
-        Position2D(0, 0),
-        Position2D(2, 0),
-        Position2D(1, 0)
-    };
-    std::vector<Position2D> desired = {
-        Position2D(0, 0),
-        Position2D(1, 0),
-        Position2D(2, 0),
-        Position2D(3, 0),
-    };
-    AntColonySystem solver;
-    std::vector<Position2D> solution = solver.solve(points);
     return 0;
 }
