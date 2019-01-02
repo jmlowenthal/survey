4d3a613bdc125eae4bd25a270022a4b1992c8a5e
jmlowenthal <jamie.lowenthal@gmail.com>
2019-01-02 11:45:00 +0000

    Add edge accessors to EDF

--- include/EuclideanDistanceFunctor.h
+++ include/EuclideanDistanceFunctor.h
@@ -8,6 +8,7 @@ template<typename Graph, typename T>
 class EuclideanDistanceFunctor {
 private:
     typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef typename boost::graph_traits<Graph>::edge_descriptor Edge;
 public:
     typedef std::pair<Vertex, Vertex> key_type;
     typedef const T reference;
@@ -15,6 +16,7 @@ public:
     typedef boost::readable_property_map_tag category;
     EuclideanDistanceFunctor(const Graph&);
     const T operator[](std::pair<Vertex, Vertex>) const;
+    const T operator[](Edge) const;
 };
 
 template<typename Graph>
@@ -25,6 +27,7 @@ private:
     const Graph& _g;
 
     typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef typename boost::graph_traits<Graph>::edge_descriptor Edge;
 
 public:
 
@@ -42,6 +45,14 @@ public:
         float dy = second.y - first.y;
         return sqrtf(dx * dx + dy * dy);
     }
+
+    const float operator[](Edge e) const {
+        boost::simple_point<float> first = _g[source(e, _g)];
+        boost::simple_point<float> second = _g[target(e, _g)];
+        float dx = second.x - first.x;
+        float dy = second.y - first.y;
+        return sqrtf(dx * dx + dy * dy);
+    }
     
 };
 
@@ -54,6 +65,7 @@ private:
     const Graph& _g;
 
     typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef typename boost::graph_traits<Graph>::edge_descriptor Edge;
 
 public:
 
@@ -64,14 +76,38 @@ public:
     
     EuclideanDistanceFunctor(const Graph& g) : _g(g) {}
 
-    const float operator[](key_type p) const {
+    const double operator[](key_type p) const {
         boost::simple_point<double> first = _g[p.first];
         boost::simple_point<double> second = _g[p.second];
         double dx = second.x - first.x;
         double dy = second.y - first.y;
         return sqrt(dx * dx + dy * dy);
     }
+
+    const double operator[](Edge e) const {
+        boost::simple_point<double> first = _g[source(e, _g)];
+        boost::simple_point<double> second = _g[target(e, _g)];
+        double dx = second.x - first.x;
+        double dy = second.y - first.y;
+        return sqrt(dx * dx + dy * dy);
+    }
     
 };
+
+template<typename Graph, typename T>
+inline typename EuclideanDistanceFunctor<Graph, T>::reference get(
+    const EuclideanDistanceFunctor<Graph, T>& edf,
+    typename EuclideanDistanceFunctor<Graph, T>::key_type p
+) {
+    return edf[p];
+}
+
+template<typename Graph, typename T>
+inline typename EuclideanDistanceFunctor<Graph, T>::reference get(
+    const EuclideanDistanceFunctor<Graph, T>& edf,
+    typename boost::graph_traits<Graph>::edge_descriptor e
+) {
+    return edf[e];
+}
 
 #endif
