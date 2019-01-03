d2137352f46f86cdd9f747b1175ba4b2f3a6fd80
jmlowenthal <jamie.lowenthal@gmail.com>
2019-01-03 20:09:42 +0000

    Fix map_property_map

--- include/map_property_map.h
+++ include/map_property_map.h
@@ -3,25 +3,31 @@
 
 #include <boost/property_map/property_map.hpp>
 #include <boost/concept/assert.hpp>
+#include <boost/smart_ptr/shared_ptr.hpp>
 #include <map>
+#include <memory>
 
 template<typename Key, typename Value>
 class map_property_map
-        : public boost::associative_property_map<std::map<Key, Value>> {
+        : public boost::put_get_helper<Value&, map_property_map<Key, Value>> {
 private:
 
-    typedef boost::associative_property_map<std::map<Key, Value>> super;
-    std::map<Key, Value> _map;
+    boost::shared_ptr<std::map<Key, Value>> _map;
 
 public:
 
-    map_property_map() : super(_map) {};
-    map_property_map(std::map<Key, Value>& map) : _map(map), super(_map) {};
+    map_property_map() : _map(new std::map<Key, Value>) {};
+    map_property_map(std::map<Key, Value>& map)
+        : _map(new std::map<Key, Value>(map)) {};
 
     typedef Key key_type;
     typedef Value value_type;
     typedef Value& reference;
     typedef boost::lvalue_property_map_tag category;
+
+    reference operator[](const key_type& k) const {
+        return (*_map)[k];
+    }
 
 };
 
