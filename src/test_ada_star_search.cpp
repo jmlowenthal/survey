83007c5d7bf879963bd2a863bf4c1dfc99f5fadb
jmlowenthal <jamie.lowenthal@gmail.com>
2019-01-06 11:39:34 +0000

    Improve tests

--- src/test_ada_star_search.cpp
+++ src/test_ada_star_search.cpp
@@ -14,17 +14,22 @@ typedef simple_point<float> pos;
 typedef adjacency_list<vecS, vecS, bidirectionalS, pos> G;
 typedef graph_traits<G>::vertex_descriptor V;
 
-TEST_CASE("[ADA] Optimality", "[full]") {
+TEST_CASE("[ADA] Path tests", "[full]") {
+
+    // Generate graph
     random::mt11213b generator(0);
     G g;
-    const int N = 64;
+    const int N = 1000;
     const float radius = 10.0f;
-    V prev = add_vertex({ radius, 0.0f }, g);
+    V prev = add_vertex({ 0.0f, radius }, g);
     for (int i = 1; i < N; ++i) {
-        V next = add_vertex({ sin((float)i / N), cos((float)i / N) }, g);
+        float theta = (float)i * 2 * M_PI / N;
+        pos p = { sin(theta) * radius, cos(theta) * radius };
+        V next = add_vertex(p, g);
         add_edge(prev, next, g);
+        prev = next;
     }
-    for (int i = 0; i < 100; ++i) {
+    for (int i = 0; i < 1000; ++i) {
         V u = random_vertex(g, generator);
         V v = random_vertex(g, generator);
         add_edge(u, v, g);
@@ -32,7 +37,10 @@ TEST_CASE("[ADA] Optimality", "[full]") {
     V source = random_vertex(g, generator);
     V destination = random_vertex(g, generator);
 
+    // Initialise helpers
     EuclideanDistanceFunctor<G, float> weight_map(g);
+    
+    // Build optimal solution
     std::map<V, V> pred_map_map;
     associative_property_map<std::map<V, V>> pred_map(pred_map_map);
     dijkstra_shortest_paths(
@@ -51,35 +59,77 @@ TEST_CASE("[ADA] Optimality", "[full]") {
     desired.push_back(current);
     std::reverse(desired.begin(), desired.end());
 
-    REQUIRE(desired.size() > 0);
-
-    map_property_map<V, float> g_map = make_g(
-        source,
-        destination
-    );
-    
-    ada_star_search(
-        g,
-        source,
-        destination,
-        _weight_map=weight_map,
-        _g=g_map
-    );
-
-    std::vector<V> solution;
-    current = source;
-    while (current != destination) {
-        solution.push_back(current);
-        current = ada_star_next_step(
-            g,
-            current,
-            weight_map,
-            g_map
-        );
+    float desiredLength = 0.0f;
+    for (int i = 0; i + 1 < desired.size(); ++i) {
+        desiredLength += get(weight_map, { desired[i], desired[i + 1] });
     }
-    solution.push_back(current);
 
-    REQUIRE(solution.size() > 0);
-    REQUIRE(solution == desired);
+    SECTION("Optimal") {
+        map_property_map<V, float> g_map = make_g(source, destination);
+        ada_star_search(
+            g,
+            source,
+            destination,
+            _weight_map=weight_map,
+            _g=g_map
+        );
+
+        std::vector<V> solution;
+        current = source;
+        while (current != destination) {
+            solution.push_back(current);
+            current = ada_star_next_step(
+                g,
+                current,
+                weight_map,
+                g_map
+            );
+        }
+        solution.push_back(current);
+
+        REQUIRE(solution.size() > 0);
+
+        float solutionLength = 0.0f;
+        for (int i = 0; i + 1 < solution.size(); ++i) {
+            solutionLength += get(weight_map, { solution[i], solution[i + 1] });
+        }
+
+        REQUIRE(solutionLength == desiredLength);
+    }
+
+    SECTION("Suboptimal (ε = 1.5)") {
+        map_property_map<V, float> g_map = make_g(source, destination);
+        ada_star_search(
+            g,
+            source,
+            destination,
+            _weight_map=weight_map,
+            _g=g_map,
+            _suboptimality=1.5f
+        );
+
+        std::vector<V> solution;
+        current = source;
+        while (current != destination) {
+            solution.push_back(current);
+            current = ada_star_next_step(
+                g,
+                current,
+                weight_map,
+                g_map
+            );
+        }
+        solution.push_back(current);
+
+        REQUIRE(solution.size() > 0);
+
+        float solutionLength = 0.0f;
+        for (int i = 0; i + 1 < solution.size(); ++i) {
+            solutionLength += get(weight_map, { solution[i], solution[i + 1] });
+        }
+
+        REQUIRE(desiredLength <= solutionLength);
+        REQUIRE(solutionLength <= 1.5f * desiredLength);
+    }
 
 }
